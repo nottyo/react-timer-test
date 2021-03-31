6ef5ba9d32f2151212a6edc9ed0b47e933d9d98b
traitanit-h <traitanit.h@linecorp.com>
2021-03-31 15:55:35 +0700

    first commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,38 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, waitFor } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders click me button', () => {
+  const { getByTestId } = render(<App />);
+  expect(getByTestId('click-me')).toBeInTheDocument();
 });
+
+test('name appears when click me', async () => {
+  // given
+  const response = {
+    userId: 1,
+    id: 1,
+    title: 'nottyo',
+    completed: false,
+  }
+  const firstResponse = {
+    userId: 1,
+    id: 1,
+    title: 'abc',
+    completed: false,
+  }
+  const mockFetchResponse = {
+    json: jest.fn().mockReturnValueOnce(firstResponse).mockReturnValue(response),
+  }
+  window.fetch = jest.fn().mockReturnValue(mockFetchResponse)
+  // when
+  const { getByTestId } = render(<App />);
+  fireEvent.click(getByTestId('click-me'))
+  // then
+  await waitFor(() => {
+    expect(window.fetch).toHaveBeenCalledTimes(2);
+    expect(getByTestId('name')).toHaveTextContent('nottyo');
+  }, {
+    timeout: 3000, // number of setInterval() interval * number of retry time + 1 sec
+  })
+})
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+type AppProps = {};
 
-export default App;
+type AppState = {
+  name: string;
+  count: number;
+}
+export default class App extends React.Component<AppProps, AppState> {
+
+  constructor(props: AppProps) {
+    super(props);
+    this.state = {
+      name: '',
+      count: 9
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event: React.MouseEvent) {
+    event.preventDefault();
+    try {
+      let counter = 9;
+      const id = setInterval(async () => {
+        const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+        const json = await response.json();
+        if (counter === 0) {
+          clearInterval(id);
+          return;
+        }
+        counter--;
+        this.setState(state => ({
+            name: json.title,
+            count: counter,
+        }));
+      }, 1000);
+    } catch (error) {
+      alert('error');
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <div>
+          <span>Count: </span>
+          <span data-testid="counter">{this.state.count}</span>
+        </div>
+        <div>
+          <span>Name: </span>
+          <span data-testid="name">{this.state.name}</span>
+        </div>
+        <div>
+          <button onClick={this.handleClick} data-testid="click-me">Click Me</button>
+        </div>
+      </div>
+    );
+  }
+}
+  
